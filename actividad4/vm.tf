99bdb8e97fbbd130d16fbbe1ee83e8d3983a8525
Gerardo Martinez <162671047+ElJerryBytes@users.noreply.github.com>
2025-09-12 13:24:31 -0600

    Create vm.tf

---
+++ actividad4/vm.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "gerardo_server_terr" {
+  ami           = "ami-0b016c703b95ecbe4"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "GerardoServerTerraform"
+  }
+}
+
+output "server_name" {
+  value = aws_instance.gerardo_server_terr.tags.Name
+}
